d39081b660d3e65e3eb0aa1610a975ad2129384e
KTH-Sys <kyawthiha1610@gmail.com>
2025-09-08 08:36:31 -0700

    feat(002): add Personalized Greeting Program

---
+++ 002/src/greeting.py
@@ -0,0 +1,12 @@
+# Personalized Greeting Program
+
+# Step 1: Ask for user details
+name = input('What is your name?')
+age = input('How old are you?')
+color = input('What is your favorite color?')
+
+# Step 2: Generate a personalized greeeting message
+print("\n-----Perasonalized Greeting-----")
+print(f'Hello {name}!')
+print(f'You are {age} years old and {color} is what a beautiful color!')
+print('It is great to meet with you!')
